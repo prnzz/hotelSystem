723848c62e59736e1e84d376c2fbda02463db6f2
frenzze <redondofrenzze@gmail.com>
2026-03-22 00:39:12 +0800

    Force fix: Rebuild Apache MPM settings from scratch

--- conn/connection.php
+++ conn/connection.php
@@ -1,6 +1,14 @@
-<?php 
-$host ='localhost';
-$username ='root';
-$password ='';
-$database = 'hotelsystem'
+<?php
+// These match the names in your Railway Variables screenshot
+$host     = getenv('MYSQLHOST');
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$database = getenv('MYSQLDATABASE'); // This will be 'railway' based on your photo
+$port     = getenv('MYSQLPORT');
+
+$conn = mysqli_connect($host, $username, $password, $database, $port);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
