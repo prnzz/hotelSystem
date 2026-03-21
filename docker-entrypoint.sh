eb07d3c55e9dcbc8a17a30ccff1c6ca99498cde5
frenzze <redondofrenzze@gmail.com>
2026-03-22 00:25:13 +0800

    Fix: Dockerfile order and MPM conflict

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-# Start Apache in the foreground
+set -e
+
+# Clear any old PID files that might cause restart loops
+rm -f /var/run/apache2/apache2.pid
+
+# Start apache
 exec apache2-foreground
