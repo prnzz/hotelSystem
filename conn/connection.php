999b79f83dc33d1d9c75632be706c3827e98ce83
frenzze <redondofrenzze@gmail.com>
2026-03-21 17:32:16 +0800

    Connected to TiDB Cloud

--- conn/connection.php
+++ conn/connection.php
@@ -1,6 +1,20 @@
 <?php 
-$host ='localhost';
-$username ='root';
-$password ='';
-$database = 'hotelsystem'
+// 1. Connection Parameters
+$host     = 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com';
+$port     = 4000;
+$username = '3A8Qaxa7CLKWSYn.root';
+$password = 'QD6jztGtWC7p3jDM'; // Use your generated password
+$database = 'test'; 
+
+// 2. Initialize and Setup SSL (Required for TiDB Cloud)
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL); 
+
+// 3. Establish the Connection
+if (!mysqli_real_connect($conn, $host, $username, $password, $database, $port)) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// 4. Set Character Set
+mysqli_set_charset($conn, "utf8mb4");
 ?>
