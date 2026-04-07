731992005ad426fc757f60925e0706c7a32c3249
frenzze <redondofrenzze@gmail.com>
2026-04-07 10:24:38 +0800

    update

--- admin-php/php/delete_room.php
+++ admin-php/php/delete_room.php
@@ -4,6 +4,6 @@ $unit_id = $_GET['unit_id'];
 include('../../conn/connection.php');
 $db = mysqli_connect($host, $username, $password, $database);
 
-$deleteRoom = mysqli_query($db, "DELETE FROM roomlist WHERE unit_id = '$unit_id'");
+$deleteRoom = mysqli_query($db, "DELETE FROM units WHERE unit_id = '$unit_id'");
 
 ?>
