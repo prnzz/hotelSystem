de06286fd4fe126fa62cb3bba760b384032c9319
frenzze <redondofrenzze@gmail.com>
2026-04-07 23:34:22 +0800

    update

--- staff-php/php/save_reservation.php
+++ staff-php/php/save_reservation.php
@@ -20,7 +20,7 @@ $dbconn = mysqli_connect($host, $username, $password, $database);
 $saveCustomer = mysqli_query($dbconn, "INSERT INTO customers (guest_name, contact) VALUES ('$guest_name', '$contact')");
 $customer_id = mysqli_insert_id($dbconn);
 
-$saveReservation = mysqli_query($dbconn, "INSERT INTO reservations (customer_id, unit_id, check_in_date, expected_check_out, duration_days, total_bill, status) VALUES ('$customer_id', '$unit_id', '$check_in', '$check_out', '$duration', '$total', 'Reserved')");
+$saveReservation = mysqli_query($dbconn, "INSERT INTO reservations (customer_id, unit_id, check_in_date, expected_check_out, duration_days, total_bill, status) VALUES ('$customer_id', '$unit_id', '$check_in', '$check_out', '$duration', '$total', 'Pending')");
 
 $updateUnit = mysqli_query($dbconn, "UPDATE units SET status = 'Occupied' WHERE unit_id = '$unit_id'");
 ?>
