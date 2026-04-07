d57aa30181bd4acb99a0663039641f519450d54d
frenzze <redondofrenzze@gmail.com>
2026-04-07 10:31:46 +0800

    update

--- admin-php/php/delete_room.php
+++ admin-php/php/delete_room.php
@@ -4,6 +4,27 @@ $unit_id = $_GET['unit_id'];
 include('../../conn/connection.php');
 $db = mysqli_connect($host, $username, $password, $database);
 
-$deleteRoom = mysqli_query($db, "DELETE FROM units WHERE unit_id = '$unit_id'");
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
+if (!isset($_GET['unit_id']) || empty($_GET['unit_id'])) {
+    die("Invalid unit ID.");
+}
+
+$unit_id = mysqli_real_escape_string($db, $_GET['unit_id']);
+
+$findRoom = mysqli_query($db, "SELECT * FROM units WHERE unit_id = '$unit_id'");
+
+if (mysqli_num_rows($findRoom) > 0) {
+    $deleteRoom = mysqli_query($db, "DELETE FROM units WHERE unit_id = '$unit_id'");
+
+    if ($deleteRoom) {
+        echo "Unit deleted successfully.";
+    } else {
+        echo "Error deleting unit: " . mysqli_error($db);
+    }
+} else {
+    echo "Unit not found.";
+}
 ?>
